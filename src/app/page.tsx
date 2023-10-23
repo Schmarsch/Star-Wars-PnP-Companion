df2bb36a89ffbb247d532aca8b002901f551feaa
Mark Junglas <m.junglas@wzl.rwth-aachen.de>
2023-10-23 15:22:33 +0200

    feat(Chars): :lipstick: Improve Character Display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,34 @@
-import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
   const allCharacters = await api.character.getAll.query();
 
-  return <div>{JSON.stringify(allCharacters)}</div>;
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th className="w-1/4">Name</th>
+              <th>XP</th>
+              <th className="w-64"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* body */}
+            {allCharacters.map((character) => (
+              <tr key={character.id}>
+                <td>{character.name}</td>
+                <td>
+                  {character.TotalXP} / <span className="text-green-500">{character.AvailableXP}</span>
+                </td>
+                <td>Link to {character.name} Page</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
